2c532c9ada363d726b093b07788f79f455e45965
Karel Vuong <karelvuong@me.com>
2014-09-29 01:45:46 -0400

    Init..

--- js/app.js
+++ js/app.js
@@ -2,46 +2,37 @@
 
 // Declare app level module which depends on filters, and services
 angular.module('karel', [
-    'ui.router',
-    'headroom',
-    'karel.controllers',
-    'karel.directives'
+  'ui.router',
+  'karel.controllers',
+  'karel.directives'
 ])
 
 .run(function($rootScope, $state){
-    $rootScope.$on("$viewContentLoaded", function(){
-        $('html body').scrollTop(0);
-        $rootScope.state = $state.$current.self.name;
-    });
+  $rootScope.$on("$viewContentLoaded", function(){
+    $('html body').scrollTop(0);
+    $rootScope.state = $state.$current.self.name;
+  });
 })
 
 .config(function($locationProvider, $stateProvider, $urlRouterProvider) {
-    $locationProvider.html5Mode(true);
-    $urlRouterProvider.otherwise("/");
+  $locationProvider.html5Mode(true);
+  $urlRouterProvider.otherwise("/");
 
-    $stateProvider
-    .state('home', {
-        url: "/",
-        templateUrl: "/partials/main.html"
-    })
-    .state('about', {
-        url: "/about",
-        templateUrl: "/partials/about.html"
-    })
-    .state('blog', {
-        url: "/blog",
-        templateUrl: "/partials/blog.html"
-    })
-    .state('post-test', {
-        url: "/blog/2014/07/11/blackberry-here-to-stay",
-        templateUrl: "/partials/blog/20140711-blackberry-here-to-stay.html"
-    })
-    .state('work', {
-        url: "/work",
-        templateUrl: "/partials/work.html"
-    })
-    .state('construction', {
-        url: "/construction",
-        templateUrl: "/partials/construction.html"
-    })
+  $stateProvider
+  .state('home', {
+    url: "/",
+    views: {
+      "page": {
+        templateUrl: "/views/home.html"
+      }
+    }
+  })
+  .state('construction', {
+    url: "/construction",
+    views: {
+      "page": {
+        templateUrl: "/views/construction.html"
+      }
+    }
+  })
 });
--- js/controllers.js
+++ js/controllers.js
@@ -3,55 +3,100 @@
 angular.module('karel.controllers', [])
 
 .controller('ApplicationController', function($scope) {
-    $scope.feed = [
-        {
-            "name": "BlackBerry Is Here To Stay",
-            "url": "/blog/2014/07/11/blackberry-here-to-stay",
-            "external": false,
-            "type": "blog",
-            "date": 1405051200000
-        },
-        {
-            "name": "1 Million Music Moments",
-            "url": "http://1MMM.org",
-            "external": true,
-            "type": "work",
-            "img": "/img/work/1MMM-cover.jpg"
-        },
-        {
-            "name": "Flow",
-            "url": "/construction",
-            "external": false,
-            "type": "work",
-            "img": "/img/work/flow-cover.jpg"
-        },
-        {
-            "name": "Dimensions",
-            "url": "http://dimensions.karel.ca",
-            "external": true,
-            "type": "work",
-            "img": "/img/work/dimensions-cover.jpg"
-        },
-        {
-            "name": "Clinico",
-            "url": "http://ctdesign.challengepost.com/submissions/17791-clinico",
-            "external": true,
-            "type": "work",
-            "img": "/img/work/clinico-cover.jpg"
-        },
-        {
-            "name": "Cooler",
-            "url": "http://bit.ly/coolerbb",
-            "external": true,
-            "type": "work",
-            "img": "/img/work/cooler-cover.jpg"
-        },
-        {
-            "name": "Cella",
-            "url": "http://bit.ly/cellabbw",
-            "external": true,
-            "type": "work",
-            "img": "/img/work/cella-cover.jpg"
-        }
-    ]
+  $scope.feed = [
+    {
+      "name": "Toronto Youth Cabinet",
+      "description": "The City of Toronto's official youth advisory body since 1998.",
+      "url": "http://thetyc.ca",
+      "external": true,
+      "tags": [
+        "work",
+        "web"
+      ],
+      "img": "/img/work/tyc-cover.jpg"
+    },
+    {
+      "name": "1 Million Music Moments",
+      "url": "http://1MMM.org",
+      "external": true,
+      "tags": [
+        "work",
+        "web"
+      ],
+      "img": "/img/work/1MMM-cover.jpg"
+    },
+    {
+      "name": "Flow",
+      "url": "/construction",
+      "external": false,
+      "tags": [
+        "work",
+        "web"
+      ],
+      "img": "/img/work/flow-cover.jpg"
+    },
+    {
+      "name": "Casa",
+      "url": "/construction",
+      "external": false,
+      "tags": [
+        "work",
+        "web",
+        "hackathon"
+      ],
+      "img": "/img/work/casa-cover.jpg"
+    },
+    {
+      "name": "Dimensions",
+      "url": "http://dimensions.karel.ca",
+      "external": true,
+      "tags": [
+        "work",
+        "web"
+      ],
+      "img": "/img/work/dimensions-cover.jpg"
+    },
+    {
+      "name": "Clinico",
+      "url": "http://ctdesign.challengepost.com/submissions/17791-clinico",
+      "external": true,
+      "tags": [
+        "work",
+        "mobile",
+        "contest"
+      ],
+      "img": "/img/work/clinico-cover.jpg"
+    },
+    {
+      "name": "Curo",
+      "url": "/construction",
+      "external": false,
+      "tags": [
+        "work",
+        "web",
+        "hackathon"
+      ],
+      "img": "/img/work/clinico-cover.jpg"
+    },
+    {
+      "name": "Cooler",
+      "url": "http://bit.ly/coolerbb",
+      "external": true,
+      "tags": [
+        "work",
+        "mobile"
+      ],
+      "img": "/img/work/cooler-cover.jpg"
+    },
+    {
+      "name": "Cella",
+      "url": "http://bit.ly/cellabbw",
+      "external": true,
+      "tags": [
+        "work",
+        "mobile"
+      ],
+      "img": "/img/work/cella-cover.jpg"
+    }
+  ]
 })
